0e77be57ac4a0bd2b08a7b77012e73d2682ee4ee
Janaina <jmtannus@gmail.com>
2024-06-14 11:58:55 -0300

    extension download_ES&+React/redux snippets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import MyBook from "./components/MyBook";
+import BgTexture from "./assets/brown-wooden-flooring.jpg";
+
+const BgTextureStyle = {
+  backgroundImage: `url(${BgTexture})`,
+  backgroundRepeat: "repeat",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  minHeight: "100vh",
+  width: "100%"
+};
 
 const App = () => {
-  return <div className="text-primary">app</div>;
+  return (
+    <div style={BgTextureStyle} className="w-full h-full flex justify-center items-center">
+      <MyBook />
+    </div>
+  );
 };
 
 export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: "#f98e2b",
-        secondary: "#f3f3f3"
-      }
+        secondary: "#f3f3f3",
+      },
     },
   },
   plugins: [],
-}
-
+};
