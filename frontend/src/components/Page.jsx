e41609bd6ba0929fb228655e324c12f6d04c237e
Janaina <jmtannus@gmail.com>
2024-06-14 16:17:56 -0300

    Book cover section

--- frontend/src/components/Page.jsx
+++ frontend/src/components/Page.jsx
@@ -4,9 +4,7 @@ const Page = forwardRef((props, ref) => {
   return (
     <div className="demoPage bg-white" ref={ref}>
       <h1>Page Header</h1>
-      <div className="h-full">
-        <p>{props.children}</p>
-      </div>
+      <div className="h-full">{props.children}</div>
       <p className="text-xs text-end text-black pb-4">
         Page number: {props.number}
       </p>
--- frontend/src/components/pages/Cover/Cover.jsx
+++ frontend/src/components/pages/Cover/Cover.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const Cover = () => {
+const Cover = ({ coverImg, title }) => {
+  
+  const coverImgStyle = {
+    backgroundImage: `url(${coverImg})`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    width: "100%",
+  };
+
   return (
-    <div>Cover</div>
-  )
-}
+    <div className="h-full" style={coverImgStyle}>
+      <div className="h-full flex justify-center items-center page-shadow">
+        <h1 className="text-primary text-5xl font-bold font-outline-2">
+          {title}
+        </h1>
+      </div>
+    </div>
+  );
+};
 
-export default Cover
+export default Cover;
