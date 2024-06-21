969f0316bf505e89487a2c9f90490737df599593
Janaina <jmtannus@gmail.com>
2024-06-21 15:12:25 -0300

    Service page

--- frontend/src/components/pages/Services/Services.jsx
+++ frontend/src/components/pages/Services/Services.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import ServicesBox from "./ServicesBox";
+
 
 const Services = () => {
   return (
-    <div>Services</div>
-  )
-}
+    <div className="h-full page-shadow bg-white px-10">
+      {/* Heading */}
+      <div className="flex flex-col items-center">
+        <h1 className="inline-block text-3xl text-center font-bold mb-3 border-b-2 border-primary pb-1">
+          Services
+        </h1>
+        <p className="text-sm text-slate-500 text-center md:w-[80%] mx-auto">
+          Html, Css, JavaScript, FrontEnd, Requisitos, Design Gráfico, Vendas
+        </p>
+      </div>
 
-export default Services
+      {/* Services Card */}
+      <ServicesBox />
+    </div>
+  );
+};
+
+export default Services;
