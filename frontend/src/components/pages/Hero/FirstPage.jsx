28b8dd0571437c2e4aca7ffebf7f89c5c7e56408
Janaina <jmtannus@gmail.com>
2024-06-21 11:51:29 -0300

    alteração do avatar e do texto

--- frontend/src/components/pages/Hero/FirstPage.jsx
+++ frontend/src/components/pages/Hero/FirstPage.jsx
@@ -8,30 +8,42 @@ const FirstPage = () => {
         <div className="p-10  space-y-4">
           {/* image-container */}
           <div>
-            <img src={personImg} alt="" className="w-[300px] mx-auto -my-12" />
+            <img
+              src={personImg}
+              alt="foto de Janaina"
+              className="w-[220px] mx-auto"
+            />
           </div>
           {/* text-container */}
           <div>
             <div className="space-y-3">
               <p className="uppercase">Hello!</p>
               <p className="text-4xl md:text-6xl font-bold text-black/80">
-                Me chamo Janaína
+                I´m Janna
               </p>
               <p className="text-black/75 text-left">Full Stack Developer</p>
               <p className="text-black/75">
-              Profissional multifacetada. Minha abordagem integra moda,
-            tecnologia e bem-estar, combinando paciência, carinho, criatividade
-            e atenção aos detalhes. Cada projeto conduzo com simpatia e
-            educação, refletindo meu compromisso sincero com a qualidade de vida e a
-            realização pessoal de todos que fazem parte do processo. 🌟
+                I am a versatile professional who blends fashion, technology,
+                and well-being with a unique touch. With patience, care, and
+                creativity, I move each project forward, always with kindness
+                and respect. My commitment is to ensure that everyone involved
+                achieves a high quality of life and personal satisfaction. I am
+                here to bring innovation and a special touch to everything I do.
+                Let's create something amazing together!🌟
               </p>
+              <a
+                className="inline-block primary-btn mr-4"
+                href="mailto:jmtannus@gmail.com"
+              >
+                {" "}
+                Resume
+              </a>
               <a
                 className="inline-block primary-btn "
                 href="mailto:jmtannus@gmail.com"
               >
                 {" "}
-                
-Contrate-me
+                More...
               </a>
             </div>
           </div>
