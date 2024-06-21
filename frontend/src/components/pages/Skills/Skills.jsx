97af5293ba6851b69d6c67bdda8ef2a0d0f74d4f
Janaina <jmtannus@gmail.com>
2024-06-21 14:47:20 -0300

    Skills

--- frontend/src/components/pages/Skills/Skills.jsx
+++ frontend/src/components/pages/Skills/Skills.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import SkillsLevel from "./SkillsLevel";
 
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
-}
+    <div className="h-full page-shadow bg-white">
+      <div>        {/* heading */}        <div>
+          <div className="relative">
+            <div className="text-center text-6xl xl:text-8xl font-bold text-black/5">              SKILLS            </div>
+            <h1 className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl md:text-4xl">              Skills            </h1>
+          </div>
 
-export default Skills
+        </div>
+        {/* skills sections */}
+        <div>
+          <SkillsLevel skillName="HTML" percentage={"90%"}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
