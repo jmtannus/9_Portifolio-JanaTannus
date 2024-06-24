43257d05e5f49c1cb9a62c457ebcd7b9726eafd6
Janaina <jmtannus@gmail.com>
2024-06-23 22:08:51 -0300

    Iniciando Pagina de projeto

--- frontend/src/components/pages/Projects/ProjectBox.jsx
+++ frontend/src/components/pages/Projects/ProjectBox.jsx
@@ -1,28 +1,29 @@
 import React from "react";
 
-
 const ProjectBox = ({ ProjectData }) => {
   return (
-    <div className="grid grid-cols-2 gap-4 mt-10">
-      {ProjectData.map((project, index) => {
-        return (
-          <div key={index} className="h-[160px] rounded-xl">
-            <img
-              src={project.image}
-              alt=""
-              className="w-full h-full object-cover rounded-xl"
-            />
-          </div>
-        );
-      })}
-      ;
+    <div>
+      <div className="grid grid-cols-2 gap-4 mt-10">
+        {ProjectData.map((project, index) => {
+          return (
+            <div key={index} className="h-[160px] rounded-xl">
+              <img
+                src={project.image}
+                alt=""
+                className="w-full h-full object-cover rounded-xl"
+              />
+              
+              {/* details section */}
+              <div>
+                <h1>{project.name} </h1>
+                <p>{project.description}</p>
+                <a href={project.previwLink}>View Project</a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-    // details section
-    // <div>
-    //     <h1>{project.name} </h1>
-    //     <p>{project.description}</p>
-    //     <a href={project.previwLink}>View Project</a>
-    // </div>
   );
 };
 
